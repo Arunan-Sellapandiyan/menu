169c2b234585e9c54468b9ec8759160ab8e834fa
Arunan-Sellapandiyan <143475479+Arunan-Sellapandiyan@users.noreply.github.com>
2024-01-10 12:07:14 +0530

    steps

--- src/steps/Step.js
+++ src/steps/Step.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 const messages = [
     'Learn React ⚛️',
@@ -6,20 +7,35 @@ const messages = [
   ];
 
 export default function Step(){
-    const step=2;
+    const [step,setStep]=useState(1);
+    const [open,setOpen]=useState(true);
+    function close(){
+    setOpen(!open);
+    }
+
+    function Prev(){
+       if(step>1) setStep( step- 1);
+    }
+    function Next(){
+        if (step<3)setStep(step+1);
+    }
     return(
-        <div className='steps'>
+       <>
+       <div>
+    <button className="close" onClick={close} >&times;</button>
+       </div>
+      {open && <div className='steps'>
             <div className="numbers">
-                <div className={`${step >=1 ? 'active':""}`}>1</div>
-                <div className={`${step >=2 ? 'active':""}`}>2</div>
-                <div className={`${step >=3 ? 'active':""}`}>3</div>
+                <div className={step >=1 ? 'active':""}>1</div>
+                <div className={step >=2 ? 'active':""}>2</div>
+                <div className={step >=3 ? 'active':""}>3</div>
                 </div>
             <p className="message">Step{step}:{messages[step-1]}</p>
         <div className="buttons" >
-            <button style={{backgroundColor:'greenyellow'}}>previous</button>
-            <button style={{backgroundColor:'greenyellow'}}>Next</button>
+            <button style={{backgroundColor:'greenyellow'}} onClick={Prev}>previous</button>
+            <button style={{backgroundColor:'greenyellow'}} onClick={Next}>Next</button>
             </div>
-        </div>
+        </div>}</> 
 
     )
 }
