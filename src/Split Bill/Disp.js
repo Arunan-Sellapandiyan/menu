7ae47a6fef40e21edb829e0466dddbd9b2dd7fc4
Arunan-Sellapandiyan <143475479+Arunan-Sellapandiyan@users.noreply.github.com>
2024-01-22 11:14:49 +0530

    split

--- src/Split Bill/Disp.js
+++ src/Split Bill/Disp.js
@@ -3,7 +3,7 @@ export default function Disp({amount,tip,Reset}){
     return(
         <>
         <p > <b>you pay ${amount+tip}  (${amount}+ ${tip}) </b></p>
-        <button onClick={Reset}>Reset</button>
+        {amount >0 ?<button onClick={Reset}>Reset</button> :""}
         </>
     )
 }
